f513e86714ff5521a5d5fc8a4fa6dd6815f187ca
Daniel Segovia <segoviadaniel2@gmail.com>
2019-04-12 12:11:09 -0300

    ADD: Incorporación de dotenv para no subir keys a github

---
+++ index.js
@@ -0,0 +1,11 @@
+const Twit = require('twit');
+const env = require('dotenv').config;
+
+var T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  // strictSSL:            true,     // optional - requires SSL certificates to be valid.
+});
